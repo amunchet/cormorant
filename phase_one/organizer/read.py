49c3f29c678e4dade74341c31fa010f2578df091
Home Chester <chet@chetenright.com>
2020-12-27 17:42:34 -0600

    feat: wip - continued work on sorter

--- phase_one/organizer/read.py
+++ phase_one/organizer/read.py
@@ -2,11 +2,22 @@
 """
 Reads in the genres CSV file and creates folders/moves files based on results
 """
+import os
 import sys
+import shutil
 import csv
 
-def test(filename, genres_inp):
+def load_genres(genres_csv:str):
+    """Loads in the genres CSV"""
+    with open(genres_csv) as f:
+        return [(x[0],x[3]) for x in [y.split(",") for y in f.readlines()]][1:]
+
+
+def parse_track_list(filename, genres_inp):
+    """Reads in the CSV file and returns an array of the genres to id mapping"""
     genres = {}
+    arr = {}
+
     for genre in genres_inp:
         genres[int(genre[0])] = genre[1]
 
@@ -14,7 +25,7 @@ def test(filename, genres_inp):
         try:
             return [genres[int(x.strip())] for x in inp.replace("[", "").replace("]", "").split(",")]
         except:
-            print("Problem occurred with ", inp, " : ", sys.exc_info()[1])
+            # print("Problem occurred with ", inp, " : ", sys.exc_info()[1])
             return []
 
     with open(filename) as f:
@@ -27,24 +38,32 @@ def test(filename, genres_inp):
                 if item != "":
                     combined = item
                     break
-            print(id, combined)
-            
+            arr[id] = combined
+    return arr
+
+def list_files(directory:str, tracklist:{}):
+    """Lists files in a given folder and what genre they belong to"""
+    return [(directory + "/" + x, tracklist[str(int(x.replace(".mp3.png", "")))]) for x in os.listdir(directory) if ".mp3.png" in x]
+    
 
 
+def move_file(pairs, directory:str):
+    """
+    Inputs:
+        :pairs - Array of tuples in format (path, genre)
+        :directory - location to place sorted images
 
-def load_genres(genres_csv:str):
-    """Loads in the genres CSV"""
-    with open(genres_csv) as f:
-        return [(x[0],x[3]) for x in [y.split(",") for y in f.readlines()]][1:]
-
+    Moves the file to the correct folder location if it exists.
+    Also creates folder if it doesn't exist.
+    """
+    
+    for (path, genre) in pairs:
+        
 
 
 if __name__ == "__main__":
-
     genres = load_genres("/mnt/sdf/genres.csv")
-    arr = test("/mnt/sdf/head.csv", genres)
+    # arr = parse_track_list("/mnt/sdf/head.csv", genres)
+    tracklist = parse_track_list("/mnt/sdf/tracks.csv", genres)
 
-    print("-------------")
-
-    print(arr)
-    print(len(arr))
+    print(list_files("/mnt/sdf/fma_large/000", tracklist))
