c4749b6a28f0324d4ae119aa2df598ee878c1641
Home Chester <chet@chetenright.com>
2020-12-27 01:22:45 -0600

    feat: added converter starting point

---
+++ phase_one/converter/README.md
@@ -0,0 +1,4 @@
+# Converter
+Converts the MP3 files from the FMA-Large dataset to Wav files. 
+
+Will also make a couple copies of them as we copy them
---
+++ phase_one/converter/convert.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Converts Mp3 files to Wav - also repeats them a given number of times
+"""
+import os
+import random
+
+def run_ffmpeg(fname:str):
+    """Runs the ffmpeg and copies the file a random number of times"""
+    filename = fname
+    if ".mp3" in fname:
+        filename = filename.replace(".mp3", "")
+
+    repl = """"concat:"""
+
+    for i in range(0, int((random.random() * 10) % 4)+1):
+        repl += """/workspace/{}.mp3|""".format(filename)
+    
+    print("Repeated ", i, " times")
+
+    repl += """/workspace/{}.mp3""".format(filename)
+    
+    repl += "\""
+
+    print("Starting up program for ", fname)
+
+    a = """docker run --rm --runtime=nvidia --volume $PWD:/workspace jrottenberg/ffmpeg:4.1-nvidia -nostats -loglevel 0 -hwaccel cuvid -i {} /workspace/{}.wav""".format(repl, filename)
+    print(a)
+    os.system(a)
+            
+if __name__ == "__main__":
+    for filename in [x for x in os.listdir(".") if ".mp3" in x]:
+        print("Running for", filename)
+        run_ffmpeg(filename)
+                    
+
+
+
