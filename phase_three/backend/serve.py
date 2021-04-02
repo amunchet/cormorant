c92d1aab7f4c36e55c87cb1ac0cc025fb6d0ab99
Automatic Linting <data@altamontco.com>
2021-04-02 15:49:53 -0500

    feat: wip - starting point for serve

--- phase_three/backend/serve.py
+++ phase_three/backend/serve.py
@@ -2,3 +2,57 @@
 """
 Basic Flask service for backend functions 
 """
+
+import functools
+import os
+import datetime
+
+from flask import Flask, request, send_file, send_from_directory
+from flask_cors import CORS
+
+
+from werkzeug.utils import secure_filename
+import uuid
+import json
+
+
+from dotenv import load_dotenv
+
+import pymongo
+from bson.objectid import ObjectId
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+# Load dot-env
+
+
+load_dotenv(verbose=True)
+
+if os.environ.get("MONGO_USERNAME") == "":
+    os.environ["MONGO_USERNAME"] = "root"
+if os.environ.get("MONGO_PASSWORD") == "":
+    os.environ["MONGO_PASSWORD"] = "temp"
+if os.environ.get("MONGO_HOST") == "":
+    os.environ["MONGO_HOST"] = "mongo"
+
+
+# Mongo Access
+mongo_client = pymongo.MongoClient(
+    "mongodb://{}:{}@{}:27017".format(
+        os.environ.get("MONGO_USERNAME"),
+        os.environ.get("MONGO_PASSWORD"),
+        os.environ.get("MONGO_HOST"),
+    )
+)
+
+@app.route("/version")
+def version():
+    return "0.0.1"
+
+if __name__ == "__main__":  # pragma: no cover
+    app.debug = True
+    app.config["ENV"] = "development"
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7000)))
