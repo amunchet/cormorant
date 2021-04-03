a50d6ccd294554183e30b100326e3d1b86e70868
Automatic Linting <data@altamontco.com>
2021-04-03 18:58:19 -0500

    chore: adding util function to populate first generation

---
+++ phase_three/utils/README.md
@@ -0,0 +1,2 @@
+# Utils
+We need a simple utility to load in the first generation into the database from a playlist (e.g. `Not Added Yet`).  Only need `title` and `youtube_link`
---
+++ phase_three/utils/dump.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Dumps in a given playlist into the Mongo Database as Generation 0
+"""
+
+if __name__ == '__main__':
+    # Clear the database first 
+
+    # Download list from the playlist URL
+
+    # Save to database
