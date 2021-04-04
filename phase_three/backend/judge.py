06ba03cd4bc78f04fc569e654dd7c2e0eb3cd03d
Automatic Linting <data@altamontco.com>
2021-04-04 17:25:07 -0500

    chore: added some verbosity to judge

--- phase_three/backend/judge.py
+++ phase_three/backend/judge.py
@@ -13,11 +13,12 @@ def judge(image_path, model_path) -> bool:
     """Judges a Path according to the given model"""
     learn = load_learner(model_path)
     try:
-        output = learn.predict(image_path)[0]
+        output = learn.predict(image_path)
+        print(output)
+        output = output[0]
     except Exception:
         print("ERROR")
         print(sys.exc_info()[1])
         return -1
-    
     return output == 'yes'
 
