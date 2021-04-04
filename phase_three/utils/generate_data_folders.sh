06e01bcef81aea493484fe0f0ccbc3bf81346a6a
Automatic Linting <data@altamontco.com>
2021-04-04 16:38:14 -0500

    chore: added utils to create folder structure

---
+++ phase_three/utils/generate_data_folders.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# To generate the structure of the data folders, since it won't be included in git
+
+mkdir /src/data/
+mkdir /src/data/incoming
+mkdir /src/data/models
+mkdir /src/data/no_holding
+
+mkdir /src/data/training
+mkdir /src/data/training/no
+mkdir /src/data/training/yes
+
+
