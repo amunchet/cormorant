add313e73932a52ea44866b38040b25f683db92b
Automatic Linting <data@altamontco.com>
2021-04-04 14:59:45 -0500

    test: corrected updated model path

--- phase_three/backend/tests/test_judge.py
+++ phase_three/backend/tests/test_judge.py
@@ -10,9 +10,9 @@ import judge
 
 def test_model_exists():
     """Tests that the given model exists"""
-    print("If this test fails, then `models/starting_point.pkl` does not exist.  It is not included in the repository since it takes up too much space.")
+    print("If this test fails, then `models/current.pkl` does not exist.  It is not included in the repository since it takes up too much space.")
     
-    assert os.path.exists("models/starting_point.pkl")
+    assert os.path.exists("/src/data/models/current.pkl")
 
 def test_judge_song():
     """
@@ -20,9 +20,9 @@ def test_judge_song():
         - Going to just pass in the raw image to judge.  Processing or acquisition are for other parts of the pipeline.
     """
     
-    assert judge.judge("tests/yes.png", "models/starting_point.pkl")
+    assert judge.judge("tests/yes.png", "/src/data/models/current.pkl")
 
-    assert not judge.judge("tests/no.png", "models/starting_point.pkl")
+    assert not judge.judge("tests/no.png", "/src/data/models/current.pkl")
 
 
-    assert judge.judge("tests/test_cron.py", "models/starting_point.pkl") == -1
+    assert judge.judge("tests/test_cron.py", "/src/data/models/current.pkl") == -1
